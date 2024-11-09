a9b047a3267a2c76c6ff118b9d2f8e2561af8842
Anurag Dhamala <anuragdhamala8@gmail.com>
2024-11-09 20:04:47 +0545

    feat: basic homepage layout

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -14,7 +14,37 @@ class _HomepageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      appBar: Header(),
-    );
+        appBar: Header(),
+        body: Center(
+            child: Column(children: [
+          SizedBox(
+            height: 200,
+          ),
+          Text(
+            "What is your personality type ?",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 500,
+            child: Column(
+              children: [
+                Expanded(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [Text("INFP"), Text("INTP")],
+                )),
+                Expanded(
+                    child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [Text("INTJ"), Text("INFJ")],
+                ))
+              ],
+            ),
+          )
+        ])));
   }
 }
