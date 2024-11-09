a484382c98afe12576cc944f676609fb2e9a24dc
Anurag Dhamala <anuragdhamala8@gmail.com>
2024-11-09 20:29:24 +0545

    scroll overflow fixes

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -15,83 +15,105 @@ class _HomepageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: const Header(),
-        body: Center(
-            child: Column(children: [
-          const SizedBox(
-            height: 150,
-          ),
-          const Text(
-            "What is your personality type ?",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            width: double.infinity,
-            height: 500,
-            child: Column(
-              children: [
-                Expanded(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width / 3,
-                      height: MediaQuery.of(context).size.height / 2,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2.0),
-                      ),
-                      child: const Text("INFP"),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width / 3,
-                      height: MediaQuery.of(context).size.height / 2,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2.0),
-                      ),
-                      child: const Text("INTP"),
-                    ),
-                  ],
-                )),
-                const SizedBox(
-                  height: 10,
-                ),
-                Expanded(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width / 3,
-                      height: MediaQuery.of(context).size.height / 2,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2.0),
-                      ),
-                      child: const Text("INFJ"),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width / 3,
-                      height: MediaQuery.of(context).size.height / 2,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2.0),
-                      ),
-                      child: const Text("INTJ"),
-                    ),
-                  ],
-                ))
-              ],
+        body: SingleChildScrollView(
+          child: Center(
+              child: Column(children: [
+            const SizedBox(
+              height: 150,
             ),
-          )
-        ])));
+            const Text(
+              "What is your personality type ?",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 400,
+              child: Column(
+                children: [
+                  Expanded(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                           print("Tapped infp");
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width / 2.2,
+                          height: MediaQuery.of(context).size.height / 2,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2.0),
+                          ),
+                          child: const Text("INFP"),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          print("Tapped intp");
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width / 2.2,
+                          height: MediaQuery.of(context).size.height / 2,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2.0),
+                          ),
+                          child: const Text("INTP"),
+                        ),
+                      ),
+                    ],
+                  )),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          print("Tapped infj");
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width / 2.2,
+                          height: MediaQuery.of(context).size.height / 2,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2.0),
+                          ),
+                          child: const Text("INFJ"),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          print("Tapped intj");
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width / 2.2,
+                          height: MediaQuery.of(context).size.height / 2,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 2.0),
+                          ),
+                          child: const Text("INTJ"),
+                        ),
+                      ),
+                    ],
+                  ))
+                ],
+              ),
+            )
+          ])),
+        ));
   }
 }
