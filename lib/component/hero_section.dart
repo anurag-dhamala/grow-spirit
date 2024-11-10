615cf5d5a43da2c3a229db3b9184d3510000dfd0
Anurag Dhamala <anuragdhamala8@gmail.com>
2024-11-10 16:10:40 +0545

    create basic hero section layout

---
+++ lib/component/hero_section.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HeroSection extends StatefulWidget {
+  const HeroSection({super.key});
+
+  @override
+  State<HeroSection> createState() {
+    return _HeroSectionState();
+  }
+}
+
+class _HeroSectionState extends State<HeroSection> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
