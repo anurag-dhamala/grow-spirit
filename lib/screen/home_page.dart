f3d726c90e3dccbb335ee18046eb0e5f9c365cf4
Anurag Dhamala <anuragdhamala8@gmail.com>
2024-11-13 07:02:57 +0545

    feat: give margin to homapage container

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:grow_spirit/component/header.dart";
+import "package:grow_spirit/component/hero_section.dart";
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -11,124 +12,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomepageState extends State<HomePage> {
-
   String selectedType = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const Header(),
-        body: SingleChildScrollView(
-          child: Center(
-              child: Column(children: [
-            const SizedBox(
-              height: 150,
-            ),
-            const Text(
-              "Choose your personality type",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 400,
-              child: Column(
-                children: [
-                  Expanded(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            selectedType = "INFP";
-                          });
-                           print("Tapped infp");
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          height: MediaQuery.of(context).size.height / 2,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.0),
-                          ),
-                          child: const Text("INFP"),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            selectedType = "INTP";
-                          });
-                          print("Tapped intp");
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          height: MediaQuery.of(context).size.height / 2,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.0),
-                          ),
-                          child: const Text("INTP"),
-                        ),
-                      ),
-                    ],
-                  )),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Expanded(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            selectedType = "INFJ";
-                          });
-                          print("Tapped infj");
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          height: MediaQuery.of(context).size.height / 2,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.0),
-                          ),
-                          child: const Text("INFJ"),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            selectedType = "INTJ";
-                          });
-                          print("Tapped intj");
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          height: MediaQuery.of(context).size.height / 2,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.0),
-                          ),
-                          child: const Text("INTJ"),
-                        ),
-                      ),
-                    ],
-                  ))
-                ],
-              ),
-            )
-          ])),
-        ));
+      appBar: const Header(),
+      body: Container(
+        margin: const EdgeInsets.all(
+          15
+        ),
+        child: const Column(
+          children: [HeroSection()],
+        ),
+      ),
+    );
   }
 }
