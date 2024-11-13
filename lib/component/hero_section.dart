9f717600828a2acfca2632c93dd7f98dc1ec73b8
Anurag Dhamala <anuragdhamala8@gmail.com>
2024-11-13 07:02:42 +0545

    feat: create hero section design

--- lib/component/hero_section.dart
+++ lib/component/hero_section.dart
@@ -12,6 +12,71 @@ class HeroSection extends StatefulWidget {
 class _HeroSectionState extends State<HeroSection> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        const Align(
+            alignment: Alignment.centerLeft,
+            child: Row(
+              children: [
+                Text(
+                  "Hello,",
+                  style: TextStyle(fontSize: 24),
+                ),
+                SizedBox(
+                  width: 2,
+                ),
+                Text(
+                  "Anurag !",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                )
+              ],
+            )),
+        const SizedBox(
+          height: 20,
+        ),
+        const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            "How are you feeling today ?",
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: List.generate(10, (index) {
+              return Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: InkWell(
+                  customBorder: const CircleBorder(
+                      eccentricity: 1,
+                      side: BorderSide(
+                        width: 30,
+                      )),
+                  onTap: () {},
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    margin: const EdgeInsets.only(
+                      right: 0,
+                    ),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(color: Colors.black, width: 2.0)),
+                    child: const Icon(
+                      Icons.accessibility_rounded,
+                      size: 50,
+                    ),
+                  ),
+                ),
+              );
+            }),
+          ),
+        )
+      ],
+    );
   }
 }
