4a38a5de0edffa512df9a1dd3e7a83bba1239fd9
Anurag Dhamala <anuragdhamala8@gmail.com>
2024-11-13 07:03:31 +0545

    add streak icon (change later)

--- lib/component/header.dart
+++ lib/component/header.dart
@@ -6,10 +6,16 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: const Text("GrowSpirit"),
+      title: const Text("GrowSpirit", style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 25
+      )),
+      leading: const Icon(Icons.accessibility, size: 30,),
+      leadingWidth: 40,
       actions: <Widget>[
           IconButton(onPressed: () {
-          }, icon: const Icon(Icons.notifications_active))
+          }, icon: const Icon(Icons.notifications_active)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.ac_unit))
       ],
     );
   }
