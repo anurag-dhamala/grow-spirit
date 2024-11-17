23f50741208fa60fc6fbcdb27d92fbdbbe9974bb
Anurag Dhamala <anuragdhamala8@gmail.com>
2024-11-17 07:11:59 +0545

    pre setup page

--- lib/screen/pre_setup_page.dart
+++ lib/screen/pre_setup_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:grow_spirit/component/header.dart';
+import 'package:grow_spirit/layout/layout.dart';
 
 class PreSetupPage extends StatefulWidget {
   const PreSetupPage({super.key});
 
   @override
   State<PreSetupPage> createState() {
-    // TODO: implement createState
     return _PreSetupPageState();
   }
 }
@@ -17,119 +16,96 @@ class _PreSetupPageState extends State<PreSetupPage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        appBar: const Header(),
-        body: SingleChildScrollView(
-          child: Center(
-              child: Column(children: [
+    return MainLayout(
+      childWidget: Center(
+        child: Column(
+          children: [
             const SizedBox(
-              height: 150,
+              height: 200,
             ),
-            const Text(
-              "Choose your personality type",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            SizedBox(
-              width: double.infinity,
-              height: 400,
-              child: Column(
-                children: [
-                  Expanded(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            selectedType = "INFP";
-                          });
-                          print("Tapped infp");
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          height: MediaQuery.of(context).size.height / 2,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.0),
-                          ),
-                          child: const Text("INFP"),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            selectedType = "INTP";
-                          });
-                          print("Tapped intp");
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          height: MediaQuery.of(context).size.height / 2,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.0),
-                          ),
-                          child: const Text("INTP"),
-                        ),
-                      ),
-                    ],
-                  )),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Expanded(
-                      child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            selectedType = "INFJ";
-                          });
-                          print("Tapped infj");
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          height: MediaQuery.of(context).size.height / 2,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.0),
-                          ),
-                          child: const Text("INFJ"),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          setState(() {
-                            selectedType = "INTJ";
-                          });
-                          print("Tapped intj");
-                        },
-                        child: Container(
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          height: MediaQuery.of(context).size.height / 2,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black, width: 2.0),
-                          ),
-                          child: const Text("INTJ"),
-                        ),
-                      ),
-                    ],
-                  ))
-                ],
+            const Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "What would you like to grow into ?",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2),
+                        ),
+                        child: const Center(
+                          child: Text("Warrior"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2),
+                        ),
+                        child: const Center(
+                          child: Text("Right warrior"),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        margin: const EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2),
+                        ),
+                        child: const Center(
+                          child: Text("Warrior"),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2),
+                        ),
+                        child: const Center(
+                          child: Text("Right warrior"),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
             )
-          ])),
-        ));
+          ],
+        ),
+      ),
+    );
   }
 }
